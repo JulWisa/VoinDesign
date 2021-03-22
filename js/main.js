a7e9e69d08de410d11cdfb5741d2d74e269fbc14
Julia <wisa@bk.ru>
2021-03-22 17:51:08 +0300

    Fix min-height for images in Safari

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 new SimpleLightbox('.gallery div div a');
 new SimpleLightbox('.gallery div .img-to-zoom a');
+
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+var imgToZoom = document.querySelectorAll("#main-container #design-boards-container .concepts .img-to-zoom img");
+if (isSafari) imgToZoom.forEach( img => img.style.minHeight = "200px");
